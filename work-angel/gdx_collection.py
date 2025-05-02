efd571181a7334f2f023fb097c7fdc003e9efa08
duarte-phy <asduarte@asu.edu>
2025-05-02 15:16:46 -0700

    data collector bugs fixed

--- work-angel/gdx_collection.py
+++ work-angel/gdx_collection.py
@@ -1,25 +1,25 @@
 '''
-This example collects data from one or more Vernier GDX devices and displays and 
-graphs the readings.
-Note that if your computer is slow, the graph may not keep up with the data collection.
-This program supports up to three sensors.
-You can stop data collection with a control-c keypress.
+This example is adapted from Vernier's GDX examples to collect data from a Go Direct motion sensor, and the data in .csv to be analyzed independently.
 
-This program uses Pyplot, which is part of MatPlotLib. Pyplot allows hundreds of options for graph style.
-See https://matplotlib.org/users/index.html and https://matplotlib.org/api/_as_gen/matplotlib.pyplot.html?highlight=pyplot 
-for information.
+From the original documents:
+
+    Note that if your computer is slow, the graph may not keep up with the data collection.
+    This program supports up to three sensors.
+    You can stop data collection with a control-c keypress.
+
+    This program uses Pyplot, which is part of MatPlotLib. Pyplot allows hundreds of options for graph style.
+    See https://matplotlib.org/users/index.html and https://matplotlib.org/api/_as_gen/matplotlib.pyplot.html?highlight=pyplot 
+    for information.
 '''
 
 import time
 import os
 import csv
-import numpy as np
+#import numpy as np #numpy not currently utilized
 import matplotlib.pyplot as plt
 
 from gdx import gdx #The gdx function calls are from a gdx.py file inside the gdx folder, which must be with this program.
 gdx = gdx.gdx()
-
-fig, ax = plt.subplots()
 
 ## Application Intro
 intro_message = "This is a working demo for godirect sensor data collection.\n"
@@ -34,7 +34,10 @@ styles = {
 style_msg = "Please select a number for a pyplot style: \n(1) ggplot \n(2) dark_background\n\n Input number: "
 style_input = str(input(style_msg))
 
+print(style_input)
 plt.style.use(styles[style_input])
+
+fig, ax = plt.subplots()
 
 # Configuration Parameters
 def config():
@@ -48,17 +51,13 @@ def config():
 
 period_in_ms, number_of_readings, time_between_readings_in_seconds = config()
 
-
-'''# CHANGE TO MATCH YOUR EXPERIMENT
-time_between_readings_in_seconds = 0.05
-number_of_readings = 100'''
 digits_of_precision = 2
 
 gdx.open(connection='usb')   # Use connection='ble' for a Bluetooth connection
 #gdx.open(connection='usb', device_to_open='GDX-FOR 071000U9')  # You can also use an argument to specify the device
 
-gdx.select_sensors([5]) # You will be asked to select the sensors to be used. You can select up to three.
-#gdx.select_sensors([1]) # You can also use an argument to select sensors. Separate multiple sensors with a comma, ([1,3])
+#gdx.select_sensors() # You will be asked to select the sensors to be used. You can select up to three.
+gdx.select_sensors([5]) # You can also use an argument to select sensors. Separate multiple sensors with a comma, ([1,3])
 
 # This gets the name and units of the sensors selected.
 column_headers = gdx.enabled_sensor_info() 
@@ -75,6 +74,7 @@ for headers in column_headers:
     unit_list.append(units)
 
 # Save the column_headers as a string, to be used in the title of the graph
+## Column Headers not currently in use, and some lines of code dependent on them have been commented out.
 column_headers_string = str(column_headers)
 column_headers_string = column_headers_string.replace("'","")
 column_headers_string = column_headers_string.replace("[","")
@@ -82,7 +82,7 @@ column_headers_string = column_headers_string.replace("]","")
 
 # Variables to store the time and readings from the sensors
 sensor_times=[]
-sensor_readings0=[]
+sensor_readings0=[] # Only one sensor is assumed to work, but option kept for multiple sensors.
 sensor_readings1=[]   
 sensor_readings2=[] 
 print_table_string = []
@@ -98,6 +98,10 @@ gdx.start(period_in_ms)
 # a "live" graph. Note that plotting to the graph during data collection may slow the data collection loop.
 def plot_graph():
     
+    # Maintains a minimum and maximum values for axes. 
+    ax.set_ylim([0, None])
+    #ax.set_ylim([0, None])
+
     # Customize the graph See Pyplot documentation
     ax.plot(sensor_times,sensor_readings0, 'ro',label=column_headers[0]) #red line for sensor 1
     
@@ -107,7 +111,7 @@ def plot_graph():
     if (number_of_sensors>2):
         ax.plot(sensor_times,sensor_readings2, color='k',label=column_headers[2]) #black line for sensor 3
 
-    plt.ylabel(column_headers_string) #name and units of the sensor selected#
+    plt.ylabel('Position(m)') #name and units of the sensor selected overwitten. Use column_headers to use with other sensors.
     plt.xlabel('Time(s)')
     plt.grid(True, alpha=0.5) #This controls whether there is a grid on the graph
     plt.pause (0.01) # display the graph briefly, as the readings are taken
@@ -115,7 +119,7 @@ def plot_graph():
 
 def print_table():
     print ("Data Table:")
-    print ('Time (s) ',column_headers_string) #label the data table that will be printed on the Python Shell
+    print ('Time (s) ','Position (m)') #label the data table that will be printed on the Python Shell
 
     # The print_table_string is a list of strings. Each element in the list contains the time and readings.
     # This variable is created in the Data Collection loop.
@@ -137,9 +141,10 @@ while not collection_complete:
         sensor_readings1=[]   
         sensor_readings2=[] 
         print_table_string = []
-        plt.cla()
+        plt.close()
+        fig, ax = plt.subplots()
         plt.style.use(styles[style_input])
-        plt.grid(True, alpha=0.5) #This controls whether there is a grid on the graph
+        #plt.grid(True, alpha=0.5) #This controls whether there is a grid on the graph
 
 
     try:
@@ -147,7 +152,7 @@ while not collection_complete:
         print ('Collecting Data...')
 
         # Print the column headers, starting with Time(s)
-        print('Time(s), ' + column_headers_string) 
+        print('Time(s), ' + 'Position(m)') 
 
         for i in range(0,number_of_readings):
             
@@ -174,7 +179,8 @@ while not collection_complete:
                 # Build a string for printing to the terminal and to be used as the title of the graph
                 round_data = str(round(data,digits_of_precision))
                 data_string = data_string + round_data + '   '
-                title_string = title_string + round_data + unit_list[d] + '   '
+                #title_string = title_string + round_data + unit_list[d] + '   '
+                #title_string = title_string + round_data + 'm' + '   '
                 d += 1
 
             # Create a list for the print_table() function. Only used for fast data collection
@@ -189,11 +195,11 @@ while not collection_complete:
 
                 # If the last reading is finished update the graph's title
                 if  i >=number_of_readings-1: 
-                    plt.title(column_headers_string +' vs '+'Time (s)')
+                    plt.title('Position (m)' +' vs '+'Time (s)')
                 
                 # If collection is in process, use the data as the graph's title, for real-time updates
                 else:
-                    plt.title(title_string)
+                    plt.title('Position (m)')
                 
                 # Call the plot_graph() function to update the graph with the new data set. 
                 plot_graph() 
@@ -212,17 +218,18 @@ while not collection_complete:
         print ('Total time for data collection ', round(time, 2), ' s')
 
         retry = bool(str(input("Type any message and enter to retry. Simply press enter to complete.")))
+
         if retry:
             collection_complete=False
         else:
             collection_complete=True
-        print(f'Retry: {retry}')
+            print(f'Retry: {retry}. \n ATTENTION: To continue, ensure that the graph window is closed.\n')
         
         # For fast collection we did not print to the graph during data collection. Now that all data
         # have been collected, send the data to the print_table() and plot_graph() function.
         if  time_between_readings_in_seconds<=0.04:
             print_table()
-            plt.title(column_headers[0]+' vs '+'Time (s)') #put a title on the top of graph
+            plt.title(column_headers[0]+' vs '+'Time (s)') #put a title on the top of graph1
             plot_graph()
         
 
@@ -238,23 +245,26 @@ gdx.close()
 plt.show()
 
 ## File name setting
-file_msg = "Enter a file name for the .csv file you are creating. For example, 'file_name.csv' without the apostrophes. \n If no file name is entered, the file will be named 'my_data.csv' \n\nEnter file name here: "
+file_msg = "Enter a file name for the .csv and .png files to be created. For example, 'file_name.csv' without the apostrophes. \n If no file name is entered, the files will be named 'my_data'.\n Enter 'x' to continue without saving. \n\nEnter file name here: "
 filename = input(file_msg)
-if len(filename) < 3:
-    filename = 'my_data.csv'
-if filename[-4:] != ".csv":
-    filename = filename + ".csv"
+if filename != 'x':
+    if len(filename) < 1:
+        filename = 'my_data'
+    fig.savefig(filename)
+    if filename[-4:] != ".csv":
+        filename = filename + ".csv"
 
-print(f' Your file name is: "{filename}".')
+    print(f' Your file name is: "{filename}".')
 
-with open('filename', 'w', newline='') as my_data_file:
-    csv_writer = csv.writer(my_data_file)
-    column_headers = ["Time (s), Position (m)"]
-    csv_writer.writerow(column_headers)
+    with open(filename, 'w', newline='') as my_data_file:
+        csv_writer = csv.writer(my_data_file)
+        column_headers = ["Time (s), Position (m)"]
+        csv_writer.writerow(column_headers)
 
-    for i in range(0,len(sensor_times)):
-        line = [round(sensor_times[i], 2), sensor_readings0[i]]
-        csv_writer.writerow(line)
+        for i in range(0,len(sensor_times)):
+            line = [round(sensor_times[i], 2), sensor_readings0[i]]
+            csv_writer.writerow(line)
 
-print("location of current working directory = ", os.getcwd()) 
-print('Free Tatooine')
+
+    print("location of current working directory = ", os.getcwd()) 
+print('The application is now closed.\n\n Free Tatooine')
